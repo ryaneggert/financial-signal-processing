91de07b466997e5ef66516a070690b970a0bccc5
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-04-29 12:25:10 -0400

    Test filters.

--- pyalgodata.py
+++ pyalgodata.py
@@ -9,22 +9,22 @@ from pyalgotrade import technical
 import technicalfilters as tf
 
 
-
-
 feed = yahoofinance.build_feed(
     ['GE', 'JCI', 'AAPL', 'ADI'], 2000, 2015, './data/historical/daily-atrader', frequency=86400, skipErrors=False)
 
 
 class MyStrategy(strategy.BacktestingStrategy):
 
-    def __init__(self, feed, instrument):
-        strategy.BacktestingStrategy.__init__(self, feed)
+    def __init__(self, feed, instrument, cash):
+        strategy.BacktestingStrategy.__init__(self, feed, cash)
         self.__rsi = rsi.RSI(feed[instrument].getCloseDataSeries(), 14)
         self.__sma = ma.SMA(self.__rsi, 15)
         self.__instrument = instrument
-        self.__EMA = tf.EMA(feed[instrument].getCloseDataSeries(), 16)
+        self.__EMA = tf.EMA(feed[instrument].getCloseDataSeries(), 16, .3)
+        self.__DT = tf.Derivative(feed[instrument])
 
     def onBars(self, bars):
+
         bar = bars[self.__instrument]
         self.info("%s %s %s %s" %
                   (bar.getClose(), self.__rsi[-1], self.__sma[-1], self.__EMA[-1]))
@@ -34,5 +34,5 @@ class MyStrategy(strategy.BacktestingStrategy):
 # feed.addBarsFromCSV("orcl", "orcl-2000.csv")
 
 # Evaluate the strategy with the feed's bars.
-myStrategy = MyStrategy(feed, "AAPL")
+myStrategy = MyStrategy(feed, 'GE', 200)
 myStrategy.run()
